ad7074fb3e209abbfec704ac6ecca136f2ec41e3
SNEHA VARSHNEY <sneha.varshney_cs19@gla.ac.in>
2021-11-12 10:24:38 +0530

    Binary Search

---
+++ Leetcode/StudyPlan/Data Structure/BinarySearch.java
@@ -0,0 +1,17 @@
+public class BinarySearch {
+    public int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length -1;
+        int mid;
+        while(start<=end){
+            mid = start + (end-start)/2;
+            if(target==nums[mid])
+                return mid;
+            else if(target<nums[mid])
+                end = mid - 1;
+            else
+                start = mid + 1;
+        }
+        return -1;
+    }
+}
