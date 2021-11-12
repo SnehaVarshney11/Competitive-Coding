9129feb5f75a8923f40f8954ead1bf1cf6d668a5
SNEHA VARSHNEY <sneha.varshney_cs19@gla.ac.in>
2021-11-12 10:22:50 +0530

    Rotate Array

---
+++ Leetcode/StudyPlan/Data Structure/RotateArray.java
@@ -0,0 +1,15 @@
+public class RotateArray {
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+         reverse(nums, 0, nums.length-1);  // reverse the whole array
+         reverse(nums, 0, k-1);  // reverse the first part
+         reverse(nums, k, nums.length-1);  // reverse the second part
+     }
+     public void reverse(int[] nums, int l, int r) {
+         while (l < r) {
+             int tmp = nums[l];
+             nums[l++] = nums[r];
+             nums[r--] = tmp;
+         }
+     }    
+}
