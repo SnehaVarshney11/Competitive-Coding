2fb17d8950adf9a3d494c89756abf3a9991f3119
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2021-11-13 10:40:41 +0530

    Two Sum 2

---
+++ Leetcode/StudyPlan/Data Structure/TwoSumII.java
@@ -0,0 +1,17 @@
+class TwoSumII{
+    public int[] twoSum(int[] numbers, int target) {
+        int a = 0, b = numbers.length-1;
+        
+        while(a<=b){
+            int sum = numbers[a]+numbers[b];
+            if(sum>target){
+                b = b-1;
+            } else if(sum<target){
+                a = a+1;
+            } else{
+                return new int[] {a+1,b+1};
+            }
+        }
+        return new int[] {a+1,b+1};
+    }
+}
