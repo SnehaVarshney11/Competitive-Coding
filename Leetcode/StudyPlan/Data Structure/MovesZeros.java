f6edd089c1f8eaaee02d0222d8094032e737e380
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2021-11-13 10:40:28 +0530

    Moves Zero

---
+++ Leetcode/StudyPlan/Data Structure/MovesZeros.java
@@ -0,0 +1,15 @@
+class MovesZeros{
+    public void moveZeroes(int[] nums) {
+        int start = 0, end = 0;
+        
+        while(end < nums.length){
+            if(nums[end] != 0){
+                int temp = nums[end];
+                nums[end] = nums[start];
+                nums[start] = temp;
+                start++;
+            }
+            end++;
+        }
+    }
+}
