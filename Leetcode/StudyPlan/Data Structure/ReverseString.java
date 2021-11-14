3c6b0b532b8d9a677033be26cd54d4b9604c681d
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2021-11-14 09:14:31 +0530

    Reverse String

---
+++ Leetcode/StudyPlan/Data Structure/ReverseString.java
@@ -0,0 +1,9 @@
+public class ReverseString {
+    public void reverseString(char[] s) {
+        for(int i=0; i<s.length/2; i++){    //Do it half the number of String length
+            char tmp = s[i];
+            s[i] = s[s.length-1-i];     //Front swap with other End side 
+            s[s.length-1-i] = tmp; 
+        }
+    }
+}
