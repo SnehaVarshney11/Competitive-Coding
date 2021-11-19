5f3675adb155c4d8575eed2f8a8e6175a9b9f6b5
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2021-11-19 16:10:27 +0530

    Merge Two Binary Trees

---
+++ Leetcode/StudyPlan/Algorithm/MergeTwoBinaryTrees.java
@@ -0,0 +1,33 @@
+package StudyPlan.Algorithm;
+
+class TreeNode{
+    int val;
+    TreeNode left, right;
+    public TreeNode(int item)
+    {
+        val = item;
+        left = right = null;
+    }
+}
+
+public class MergeTwoBinaryTrees {
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        return merge(root1, root2);
+    }
+    public TreeNode merge(TreeNode root1, TreeNode root2){
+        if(root1 == null && root2 == null)
+            return null;
+        if(root1 == null && root2 != null)
+            return root2;
+        if(root1 != null && root2 == null)
+            return root1;
+        
+        TreeNode sum = new TreeNode(root1.val + root2.val);
+        
+        sum.left = merge(root1.left , root2.left);
+        
+        sum.right = merge(root1.right , root2.right);
+        
+        return sum;
+    }
+}
