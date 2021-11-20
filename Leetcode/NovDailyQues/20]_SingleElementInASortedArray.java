b606a2f304d9ba7a707083be0f8b0f3bdb735f5d
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2021-11-20 09:59:52 +0530

    Single Element in a Sorted Array

---
+++ Leetcode/NovDailyQues/20]_SingleElementInASortedArray.java
@@ -0,0 +1,23 @@
+package NovDailyQues;
+
+class SingleElementInASortedArray {
+    public int singleNonDuplicate(int[] nums) {
+        int start = 0, end = nums.length-1;
+        
+        while(start < end){
+            int mid = start + (end - start)/2;
+            
+            if(mid % 2 == 1){
+                mid--;
+            }
+            if(nums[mid] != nums[mid+1])
+            {
+                end = mid;
+            }
+            else{
+                start = mid + 2;
+            }
+        }
+        return nums[start];
+    }    
+}
