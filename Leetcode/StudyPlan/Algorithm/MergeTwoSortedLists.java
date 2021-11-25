16d0fa8400ce110f7e662e1c83b72804c68571b7
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2021-11-25 21:42:14 +0530

    Merge Two Sorted Lists

--- Leetcode/StudyPlan/Algorithm/MergeTwoSortedLists.java
+++ Leetcode/StudyPlan/Algorithm/MergeTwoSortedLists.java
@@ -1,19 +1,38 @@
 package StudyPlan.Algorithm;
 
-
-public class MergeTwoSortedLists {
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    class ListNode{
+        int val;
+        ListNode next;
+    }
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         if(list1 == null)
             return list2;
         if(list2 == null)
             return list1;
         
-        ListNode dummy = new ListNode(0);
+        ListNode dummy = new ListNode();
         ListNode curr = dummy;
         
         while(list1 != null && list2 != null){
-            curr.next = list1;
-            list1 = list1.next;
+            if(list1.val <= list2.val){
+                curr.next = list1;
+                list1 = list1.next;
+            }
+            else{
+                curr.next = list2;
+                list2 = list2.next;
+            }
             curr = curr.next;
         }
         curr.next = list1 == null? list2:list1;
