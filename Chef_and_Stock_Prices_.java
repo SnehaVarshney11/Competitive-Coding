c84ea85513250a4e8e062e9e06bd8cc9a830164e
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2021-11-29 10:20:02 +0530

    codechef problems

--- Chef_and_Stock_Prices_.java
+++ Chef_and_Stock_Prices_.java
@@ -1,24 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 class Chef_and_Stock_Prices_{
     public static void main(String[] args) throws java.lang.Exception{
-        Scanner sc = new Scanner(System.in);
-    
-        int t = sc.nextInt();
-        while(t-->0){
-            int s = sc.nextInt();
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            int c = sc.nextInt();
-
-            int rup = s - c;
-
-            if(rup >= a && rup <= b){
+        InputStreamReader r = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(r);
+        int t = Integer.parseInt(br.readLine());
+        while(t-- != 0){
+            String str = br.readLine();
+            String words[] = str.split(" ");
+            double s = Integer.parseInt(words[0]);
+            double a = Integer.parseInt(words[1]);
+            double b = Integer.parseInt(words[2]);
+            double c = Integer.parseInt(words[3]);
+            double change = (c * s / 100);
+            double finalPrice = 0;
+            finalPrice = s + change;
+            if(finalPrice >= a && finalPrice <= b){
                 System.out.println("Yes");
-            }
-            else{
+            }else{
                 System.out.println("No");
             }
         }
+        br.close();
     }
 }
