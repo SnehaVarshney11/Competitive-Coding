26bcf3dd36d8aa41bbec80961f6da7a7cf6b1844
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2021-12-03 18:19:24 +0530

    codechef problems

--- codechef/Chef_and_Stock_Prices_.java
+++ codechef/Chef_and_Stock_Prices_.java
@@ -1,7 +1,5 @@
 import java.io.BufferedReader;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 
 class Chef_and_Stock_Prices_{
     public static void main(String[] args) throws java.lang.Exception{
