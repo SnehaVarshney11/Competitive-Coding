2f1c47440655df89326c9a2f7783cad51978a383
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2021-12-07 18:42:27 +0530

    Odd Even Linked List

---
+++ Leetcode/DecDailyQues/02]_OddEvenLinkedList.java
@@ -0,0 +1,45 @@
+package DecDailyQues;
+
+class ListNode{
+    ListNode head, next;
+}
+class OddEvenLinkedList {
+    public ListNode oddEvenList(ListNode head) {
+        if(head==null || head.next==null) return head;
+        
+        ListNode oddHead = null, oddTail = null;
+        ListNode evenHead = null, evenTail = null;
+        ListNode curr = head;
+        int i = 1;
+        while(curr!=null){
+			// generate the odd indices list
+            if(i%2==1){
+                if(oddHead==null){
+                    oddHead = curr;
+                    oddTail = curr;
+                }
+                else{
+                    oddTail.next = curr;
+                    oddTail = oddTail.next;
+                }
+            }
+			// generate the even indices list
+            else{
+                if(evenHead==null){
+                    evenHead = curr;
+                    evenTail = curr;
+                }
+                else{
+                    evenTail.next = curr;
+                    evenTail = evenTail.next;
+                }
+            }
+            curr = curr.next;
+            i++;
+        }
+        
+        evenTail.next = null;     // there should not be any node after even tail
+        oddTail.next  = evenHead;   // join even list after odd
+        return oddHead;
+    }    
+}
