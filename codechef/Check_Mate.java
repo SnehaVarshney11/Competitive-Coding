541a8fcbac43d54e4537c6a81f169ee6e9bf5fb7
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2021-12-08 19:45:12 +0530

    Check Mate

--- codechef/Check_Mate.java
+++ codechef/Check_Mate.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 class Check_Mate{
     public static void main(String[] args) throws java.lang.Exception{
         Scanner sc = new Scanner(System.in);
+        sc.close();
         int t = sc.nextInt();
         while(t-->0){
             int xk = sc.nextInt();
