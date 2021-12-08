b6dd2df5769917cc6d6a9dfa8ba4e26616b7c55c
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2021-12-08 19:56:32 +0530

    Enormous Input Test

--- codechef/Enormous_Input_Test.java
+++ codechef/Enormous_Input_Test.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 class Enormous_Input_Test{
     public static void main(String[] args) throws java.lang.Exception{
         Scanner sc = new Scanner(System.in);
+        sc.close();
         int n = sc.nextInt();
         int k = sc.nextInt();
         int count = 0;
