80db131b08a08edd481faac3fa8916993639083e
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2021-12-08 19:44:24 +0530

    ATM

---
+++ codechef/ATM.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class ATM
+{
+    public static void main(String[] args) throws java.lang.Exception{
+        Scanner sc = new Scanner(System.in);
+        int cash = sc.nextInt();
+        float balance = sc.nextFloat();
+        float charge = 0.5f;
+
+        if(cash % 5 == 0 && (cash + charge) <= balance){
+            balance = balance - cash - charge;
+        }
+        System.out.printf("%.2f",balance);
+    }
+}
