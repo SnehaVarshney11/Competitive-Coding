534f2943e4fb391d17f9fa8bcce02bbe8a622688
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2021-12-08 20:01:12 +0530

    Add Two Numbers

---
+++ codechef/Add_Two_Numbers.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Add_Two_Numbers{
+    public static void main(String[] args) throws java.lang.Exception{
+        Scanner sc = new Scanner(System.in);
+        sc.close();
+        int t = sc.nextInt();
+        while(t-->0){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int sum = a + b;
+            System.out.println(sum);
+        }
+    }
+}
