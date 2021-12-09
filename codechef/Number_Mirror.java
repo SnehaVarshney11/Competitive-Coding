770b14cb262dc0e8b550e311974c3a912d321290
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2021-12-09 19:51:44 +0530

    Number Mirror

---
+++ codechef/Number_Mirror.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class Number_Mirror{
+    public static void main(String[] args) throws java.lang.Exception{
+        Scanner sc = new Scanner(System.in);
+        sc.close();
+        int n = sc.nextInt();
+        System.out.println(n);
+    }
+}
