bf48600add77a2bf7124ab5cc5dce8eaa80ffdbc
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2021-12-09 19:56:55 +0530

    Sum of Digits

--- codechef/Sum_of_Digits.java
+++ codechef/Sum_of_Digits.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 class Sum_of_Digits{
     public static void main(String[] args) throws java.lang.Exception{
         Scanner sc = new Scanner(System.in);
+        sc.close();
         int t = sc.nextInt();
         while(t-->0){
             int n = sc.nextInt();
