4a3ce221a42336de737e1e5051e090f1a202b66a
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2021-12-13 23:12:46 +0530

    Helping Chef

---
+++ codechef/Helping_Chef.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Helping_Chef{
+    public static void main(String[] args) throws java.lang.Exception{
+        Scanner sc = new Scanner(System.in);
+        sc.close();
+        int t = sc.nextInt();
+        while(t-->0){
+            int n = sc.nextInt();
+            if(n < 10){
+                System.out.println("Thanks for helping Chef!");
+            }else{
+                System.out.println("-1");
+            }
+        }
+    }
+}
