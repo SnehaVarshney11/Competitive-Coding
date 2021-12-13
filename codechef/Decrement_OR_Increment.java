5969d3cad1670d9ab93fa8c648c1121aa8683cb5
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2021-12-13 23:17:31 +0530

    Decrement_OR_Increment

---
+++ codechef/Decrement_OR_Increment.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Decrement_OR_Increment{
+    public static void main(String[] args) throws java.lang.Exception{
+        Scanner sc = new Scanner(System.in);
+        sc.close();
+        int n = sc.nextInt();
+        if(n % 4 == 0){
+            n++;
+        }else{
+            n--;
+        }
+        System.out.println(n);
+    }
+}
