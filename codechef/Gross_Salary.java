78123e1c26df373b7718b278d99cb9a52355c9b2
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2021-12-15 23:34:52 +0530

    Gross Salary

---
+++ codechef/Gross_Salary.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class Gross_Salary{
+    public static void main(String[] args) throws java.lang.Exception{
+        Scanner sc = new Scanner(System.in);
+        sc.close();
+        int t = sc.nextInt();
+        while(t-->0){
+            double salary = sc.nextDouble();
+            double HRA = 0;
+            double DA = 0;
+            double gross = 0;
+
+            if(salary < 1500){
+                HRA = 0.1 * salary;
+                DA = 0.9 * salary;
+            }
+            if(salary >= 1500){
+                HRA = 500;
+                DA = 0.98 * salary;
+            }
+            gross = salary + HRA + DA;
+            System.out.println(gross);
+        }
+    }
+}
