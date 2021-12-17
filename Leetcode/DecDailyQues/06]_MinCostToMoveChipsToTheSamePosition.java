b47b245b1198b722b30bdd0083e1e98e3f927bde
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2021-12-17 21:43:19 +0530

    Minimum Cost to Move Chips to The Same Position

---
+++ Leetcode/DecDailyQues/06]_MinCostToMoveChipsToTheSamePosition.java
@@ -0,0 +1,13 @@
+package DecDailyQues;
+
+class MinCostToMoveChipsToTheSamePosition {
+    public int minCostToMoveChips(int[] position) { 
+        int even = 0, odd = 0;
+        
+        for(int p : position){
+            if(p%2==0) even++;
+            else odd++;
+        }                                              
+        return Math.min(even,odd);
+    }    
+}
