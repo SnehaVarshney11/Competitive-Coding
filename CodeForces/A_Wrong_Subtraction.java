100ef214105661aa50e8b7e8f6ea4d0556dbb732
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2021-12-18 21:35:11 +0530

    Wrong Subtraction

---
+++ CodeForces/A_Wrong_Subtraction.java
@@ -0,0 +1,21 @@
+package CodeForces;
+import java.util.Scanner;
+
+public class A_Wrong_Subtraction{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.close();
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int last_digit;
+        for(int i = 1; i <= k; i++){
+            last_digit = n % 10;
+            if(last_digit == 0){
+                n /= 10;
+            }else{
+                n -= 1;
+            }
+        }
+        System.out.println(n);
+    }
+}
