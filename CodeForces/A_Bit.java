b5a8d48a1f9bb65907c44360c51507961afcce4b
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2021-12-19 23:07:09 +0530

    Bit++

---
+++ CodeForces/A_Bit.java
@@ -0,0 +1,27 @@
+package CodeForces;
+import java.util.Scanner;
+
+public class A_Bit{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.close();
+        int num = sc.nextInt();
+        int x=0;
+        String a = "++X";
+        String b = "X++";
+        String[] str = new String[num];
+        for(int i=0;i<num;i++)
+        {
+            String y = sc.next();
+            str[i]=y;
+        }
+        for(int i=0;i<num;i++)
+        {
+            if(str[i].equals(a) || str[i].equals(b))
+                x++;
+            else
+                x--;
+        }
+        System.out.println(x);
+    }
+}
