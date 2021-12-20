fe5e3acf3b60b7737e71bd4ec4f0cc06860ada77
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2021-12-20 20:12:20 +0530

    Beautiful Matrix

---
+++ CodeForces/A_Beautiful_Matrix.java
@@ -0,0 +1,24 @@
+package CodeForces;
+import java.util.Scanner;
+
+public class A_Beautiful_Matrix{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+		sc.close();
+		int i = 1;
+		int j = 1;
+		for (i = 1; i <= 5; i++) {
+			if (sc.nextInt() == 1) {
+				break;
+			}
+
+			// Increment;
+			if (i == 5) {
+				j++;
+				i = 0;
+			}
+		}
+
+		System.out.println(Math.abs(i-3)+Math.abs(j-3));
+    }
+}
