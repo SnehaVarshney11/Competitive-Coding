b1e5ef392901a635d9cc4df7a1bfba5851ccfbd3
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2021-12-24 22:57:18 +0530

    Tram

--- CodeForces/A_Tram.java
+++ CodeForces/A_Tram.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class A_Tram{
     public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-        
+        sc.close();
 		int n = sc.nextInt(); // number of stops
 		int enter, exit, currentCapacity, minCapacity;
 		enter = exit = currentCapacity = minCapacity = 0;
