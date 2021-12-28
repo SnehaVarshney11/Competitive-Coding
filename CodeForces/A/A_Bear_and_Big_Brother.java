8f48b9f45a60fb2f65b986bfa0020f617465fe84
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2021-12-28 16:43:29 +0530

    Bear and Big Brother

---
+++ CodeForces/A/A_Bear_and_Big_Brother.java
@@ -0,0 +1,19 @@
+package CodeForces.A;
+import java.util.Scanner;
+
+public class A_Bear_and_Big_Brother{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.close();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int count = 0;
+        
+        while(a <= b){
+            a = a * 3;
+            b = b * 2;
+            count++;
+        }
+        System.out.println(count);
+    }
+}
