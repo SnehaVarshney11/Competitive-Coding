9eb26ff45f2d39f910efcef9ca49eb86857dcdb4
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2021-12-28 16:44:24 +0530

    Domino piling

---
+++ CodeForces/A/A_Domino_piling.java
@@ -0,0 +1,13 @@
+package CodeForces.A;
+import java.util.Scanner;
+
+public class A_Domino_piling{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        sc.close();
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int k=n*m/2;
+        System.out.println(k);
+    }
+}
