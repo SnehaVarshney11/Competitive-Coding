32c19f99be010234c1b015eb5f76c4885deaada1
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2021-12-28 16:44:31 +0530

    Elephant

---
+++ CodeForces/A/A_Elephant.java
@@ -0,0 +1,11 @@
+package CodeForces.A;
+import java.util.Scanner;
+
+public class A_Elephant{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.close();
+        int x = sc.nextInt();
+        System.out.println((x + 4) / 5);
+    }
+}
