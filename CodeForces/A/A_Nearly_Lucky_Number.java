fed026cd1f9006689e13d9872e84e05fe5aff8f7
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2021-12-28 16:44:50 +0530

    Nearly Lucky Number

---
+++ CodeForces/A/A_Nearly_Lucky_Number.java
@@ -0,0 +1,18 @@
+package CodeForces.A;
+import java.util.Scanner;
+
+public class A_Nearly_Lucky_Number{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+		sc.close();
+		String n = sc.next();
+		int count = 0;
+		for(int i=0; i<n.length(); i++)
+    		if(n.charAt(i)=='4' || n.charAt(i)=='7')
+    		    ++count;
+		if(count == 7 || count == 4)
+		    System.out.println("YES");
+		else
+		    System.out.println("NO");
+    }
+}
