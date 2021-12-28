a039aa7f4742bc949596693fd14e752b560313a3
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2021-12-28 16:46:38 +0530

    Word

---
+++ CodeForces/A/A_Word.java
@@ -0,0 +1,28 @@
+package CodeForces.A;
+import java.util.Scanner;
+
+public class A_Word{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.close();
+		String s=sc.next();
+		String sl=s.toLowerCase();
+		String su=s.toUpperCase();
+		int l=0;
+		for(int i=0;i<s.length();i++)
+		{
+			if(s.toCharArray()[i]==sl.toCharArray()[i])
+				l++;
+		}
+		int u=0;
+		for(int i=0;i<s.length();i++)
+		{
+			if(s.toCharArray()[i]==su.toCharArray()[i])
+				u++;
+		}
+		if(l>u || l==u)
+			System.out.println(s.toLowerCase());
+		else
+			System.out.println(s.toUpperCase());
+    }
+}
