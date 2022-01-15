74f0cf301fcc4c8f53cf74331ae0167e6fe9c457
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-01-15 20:19:18 +0530

    Pythagorean Triplet

---
+++ Amazon Questions/02]_PythagoreanTriplet.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+class PythagoreanTriplet {
+    boolean checkTriplet(int[] arr, int n) {
+        // square the element in array
+        for(int i = 0; i < n; i++){
+            arr[i] = arr[i] * arr[i];
+        }
+        Arrays.sort(arr);
+        for(int i = n-1; i >= 2; i--){
+            int left = 0;
+            int right = i-1;
+            
+            while(left < right){
+                if(arr[left] + arr[right] == arr[i]){
+                    return true;
+                }
+                if(arr[left] + arr[right] < arr[i]){
+                    left++;
+                }else{
+                    right--;
+                }
+            }
+        }
+        return false;
+    }
+}
