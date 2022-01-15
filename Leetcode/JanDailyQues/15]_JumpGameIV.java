5dcb299412f3ed3a37829e70641811f2f139521f
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-01-15 16:27:05 +0530

    Jump Game IV

---
+++ Leetcode/JanDailyQues/15]_JumpGameIV.java
@@ -0,0 +1,54 @@
+package JanDailyQues;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+
+class JumpGameIV {
+    public int minJumps(int[] arr) {
+        int n = arr.length;
+        
+        if(n == 1) return 0;
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        int step = 0;
+        
+        // fill the map
+        for(int i = 0; i < n; i++){
+            map.computeIfAbsent(arr[i], v -> new ArrayList<>()).add(i);
+        }
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(0);
+        
+        while(!q.isEmpty()){
+            step++;
+            int size = q.size();
+            for(int i = 0; i < size; i++){
+                int j = q.poll();
+            
+                if(j - 1 >= 0 && map.containsKey(arr[j - 1])){
+                    q.offer(j - 1);
+                }
+
+                if(j + 1 < n && map.containsKey(arr[j + 1])){
+                    if(j + 1 == n - 1) return step;
+                    q.offer(j + 1);
+                }
+
+                if(map.containsKey(arr[j])){
+                    for(int k : map.get(arr[j])){
+                        if(k != j){
+                            if(k == n - 1) return step;
+                            q.offer(k);
+                        }
+                    }
+                }
+                map.remove(arr[j]);
+            }
+        }
+        
+        return step;
+    }
+}
