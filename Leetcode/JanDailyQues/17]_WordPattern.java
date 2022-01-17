18c4e28a8e99302870ce09812027edc3e1cc7fc0
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-01-17 19:53:37 +0530

    Word Pattern

---
+++ Leetcode/JanDailyQues/17]_WordPattern.java
@@ -0,0 +1,17 @@
+package JanDailyQues;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class WordPattern {
+    public boolean wordPattern(String pattern, String s) {
+        String[] words = s.split(" ");
+        if (words.length != pattern.length())
+            return false;
+        Map index = new HashMap();
+        for (Integer i=0; i<words.length; ++i)
+            if (index.put(pattern.charAt(i), i) != index.put(words[i], i))
+                return false;
+        return true;
+    }
+}
