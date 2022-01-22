1db31a20ae1442ae4e026370ce9a1ed947994038
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-01-22 18:02:43 +0530

    Stone Game IV

---
+++ Leetcode/JanDailyQues/22]_StoneGameIV.java
@@ -0,0 +1,19 @@
+package JanDailyQues;
+
+class StoneGameIV {
+    Boolean[] dp = new Boolean[100001];
+
+        public boolean winnerSquareGame(int n) {
+            if (n == 0) return false;
+            if (dp[n] != null) return dp[n];
+            boolean winner = false;
+            for (int i = 1; i * i <= n; i++) {
+                if (!winnerSquareGame(n - i * i)) {
+                    winner = true;
+                    break;
+
+                }
+            }
+            return dp[n] = winner;
+        }
+}
