5accaeb611eb56759ae90690088f4c6c95f006fe
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-01-29 18:03:56 +0530

    Largest Rectangle in Histogram

---
+++ Leetcode/JanDailyQues/29]_LargestRectangleInHistogram.java
@@ -0,0 +1,49 @@
+package JanDailyQues;
+
+import java.util.Stack;
+
+class LargestRectangleInHistogram {
+    public int largestRectangleArea(int[] heights) {
+        Stack<Integer> s = new Stack<Integer>();
+        int n = heights.length;
+        int max_area = 0;
+        int tp, area_with_top;
+        int i=0;
+        while (i < n)
+        {
+            // If this bar is higher than the bar on top stack, push it to stack
+            if (s.empty() || heights[s.peek()] <= heights[i])
+                s.push(i++);
+      
+            // If this bar is lower than top of stack, then calculate area of rectangle
+            // with stack top as the smallest (or minimum height) bar. 'i' is
+            // 'right index' for the top and element before top in stack is 'left index'
+            else
+            {
+                tp = s.peek();  // store the top index
+                s.pop();  // pop the top
+      
+                // Calculate the area with hist[tp] stack as smallest bar
+                area_with_top = heights[tp] * (s.empty() ? i : i - s.peek() - 1);
+      
+                // update max area, if needed
+                if (max_area < area_with_top)
+                    max_area = area_with_top;
+            }
+        }
+      
+        // Now pop the remaining bars from stack and calculate area with every
+        // popped bar as the smallest bar
+        while (s.empty() == false)
+        {
+            tp = s.peek();
+            s.pop();
+            area_with_top = heights[tp] * (s.empty() ? i : i - s.peek() - 1);
+      
+            if (max_area < area_with_top)
+                max_area = area_with_top;
+        }
+      
+        return max_area;
+    }
+}
