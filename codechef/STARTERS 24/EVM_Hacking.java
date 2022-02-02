27059145a9019fe4fbd08553eb235622934f0453
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-02-02 21:45:00 +0530

    EVM Hacking

---
+++ codechef/STARTERS 24/EVM_Hacking.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class EVM_Hacking{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            int p = sc.nextInt();
+            int q = sc.nextInt();
+            int r = sc.nextInt();
+
+            int half = (p + q + r)/2;
+            if(p+b+c > half || a+q+c > half || a+b+r > half){
+                System.out.println("YES");
+            }else{
+                System.out.println("NO");
+            }
+        }
+    }
+}
