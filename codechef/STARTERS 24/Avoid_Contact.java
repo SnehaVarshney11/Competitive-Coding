1b30f1f98bc1739815c5f13fe84e16f006150ff0
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-02-02 21:45:48 +0530

    Avoid Contact

---
+++ codechef/STARTERS 24/Avoid_Contact.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Avoid_Contact{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            int ans = 0;
+
+            if(y == 0){
+                System.out.println(x);
+            }
+            else if(x == y){
+                System.out.println(2 * y - 1);
+            }
+            else{
+                ans = (2 * y) + (x-y);
+                System.out.println(ans);
+            }
+        }
+    }
+}
