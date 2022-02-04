f5783f20b3be305bfb74e5390644879cba0e9ad6
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-02-04 11:31:57 +0530

    Print Vertical Order of Binary Tree

--- Tree/25]_PrintVerticalOrder.java
+++ Tree/25]_PrintVerticalOrder.java
@@ -1,8 +1,12 @@
 package Tree;
 
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
+
 class Node{
     Node left, right;
-    int data, height;
+    int data;
 }
 class PrintVerticalOrder {
     public Node createNode(int val){
@@ -12,8 +16,42 @@ class PrintVerticalOrder {
         node.right = null;
         return node;
     }
-    public void verticalOrder(Node node){
+    public void printverticalOrder(Node node){
         if(node == null) return;
+        int height = 0;
+
+        TreeMap<Integer, ArrayList<Integer>> tm = new TreeMap<>();
+        getVerticalOrder(node, height, tm);
+
+        for (Map.Entry<Integer, ArrayList<Integer>> cm : tm.entrySet()) {
+            System.out.println(cm.getValue());
+          }
+    }
+    public void getVerticalOrder(Node node, int hd, TreeMap<Integer, ArrayList<Integer>> m) {
+        if (node == null) {
+          return;
+        }
+    
+        if (m.get(hd) == null) {
+          ArrayList<Integer> l = new ArrayList<Integer>();
+          l.add(node.data);
+          m.put(hd, l);
+        } else {
+          ArrayList<Integer> l = m.get(hd);
+          l.add(node.data);
+          m.put(hd, l);
+        }
+    
+        getVerticalOrder(node.left, hd - 1, m);
+        getVerticalOrder(node.right, hd + 1, m);
+    }
+    
+    public Node createNewNode(int val) {
+      Node newNode = new Node();
+      newNode.data = val;
+      newNode.left = null;
+      newNode.right = null;
+      return newNode;
     }
     public static void main(String[] args) {
         PrintVerticalOrder a = new PrintVerticalOrder();
@@ -28,6 +66,6 @@ class PrintVerticalOrder {
         root.right.right = a.createNode(9);
         root.right.right.left = a.createNode(4);
 
-        a.verticalOrder(root);
+        a.printverticalOrder(root);
     }
 }
