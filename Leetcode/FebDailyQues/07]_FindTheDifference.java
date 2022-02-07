d892da33e324e16cfd3fe02d0da99d436cb82595
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-02-07 10:33:01 +0530

    Find the Difference

---
+++ Leetcode/FebDailyQues/07]_FindTheDifference.java
@@ -0,0 +1,10 @@
+package FebDailyQues;
+
+class FindTheDifference {
+    public char findTheDifference(String s, String t) {
+        char c = 0;
+        for(char cs : s.toCharArray()) c ^= cs;
+        for(char ct : t.toCharArray()) c ^= ct;
+        return c;
+    }
+}
