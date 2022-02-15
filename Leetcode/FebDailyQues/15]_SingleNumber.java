cdb71e747e3227cff6daaad18544356fcc22520d
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-02-15 19:43:58 +0530

    Single Number

---
+++ Leetcode/FebDailyQues/15]_SingleNumber.java
@@ -0,0 +1,20 @@
+package FebDailyQues;
+
+import java.util.Arrays;
+
+class SingleNumber {
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        int a = nums[0];
+        for(int i=0;i<nums.length-1;i+=2){
+            if(nums[i]!=nums[i+1]){
+                a = nums[i];
+                break;
+            }
+            else{
+                a = nums[nums.length-1];
+            }
+        }
+        return a;
+    }
+}
