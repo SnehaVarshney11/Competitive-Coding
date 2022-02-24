baa0b7b90a6299d04730a408b5c7298e74236685
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-02-24 22:31:17 +0530

    commit

---
+++ Blackjack.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Blackjack{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            if(21 - (a+b) > 10){
+                System.out.println("-1");
+            }else{
+                System.out.println(21-(a+b));
+            }
+        }
+    }
+}
---
+++ Leetcode/FebDailyQues/ListNode.java
@@ -0,0 +1,30 @@
+package FebDailyQues;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
+class SortList {
+    public ListNode sortList(ListNode head) {
+        ListNode Fake = new ListNode(-1);
+    ArrayList<Integer> list = new ArrayList();
+    ListNode temp = head;
+    while(temp!=null){
+        list.add(temp.val); 
+        temp=temp.next;
+    }
+    Collections.sort(list);
+    ListNode FakeHead = Fake;
+    for(int i =0; i<list.size(); i++){
+        FakeHead.next = new ListNode(list.get(i));
+        FakeHead = FakeHead.next;
+    }
+    return Fake.next;
+}
+}
