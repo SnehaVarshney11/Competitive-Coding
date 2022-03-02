afb66ec1fc11ff1e7bc143229156cd2d962e9fcd
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-03-02 22:11:23 +0530

    Is Subsequence

---
+++ Leetcode/MarchDailyQues/02]_IsSubsequence.java
@@ -0,0 +1,14 @@
+package MarchDailyQues;
+
+class IsSubsequence {
+    public boolean isSubsequence(String s, String t) {
+        int i = 0, j = 0;
+        while(i < s.length() && j < t.length()){
+            if(s.charAt(i) == t.charAt(j)){
+                i++;
+            }
+            j++;
+        }
+        return i == s.length();
+    }
+}
