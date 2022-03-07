8319dae405a552cae5ccce2d3ec8c13612b96789
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-03-07 18:52:39 +0530

    Merge Two Sorted Lists

---
+++ Leetcode/MarchDailyQues/07]_MergeTwoSortedLists.java
@@ -0,0 +1,34 @@
+package MarchDailyQues;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+class MergeTwoSortedLists {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if(list1 == null)
+            return list2;
+        if(list2 == null)
+            return list1;
+        
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        
+        while(list1 != null && list2 != null){
+            if(list1.val <= list2.val){
+                curr.next = list1;
+                list1 = list1.next;
+            }
+            else{
+                curr.next = list2;
+                list2 = list2.next;
+            }
+            curr = curr.next;
+        }
+        curr.next = list1 == null? list2:list1;
+        return dummy.next;
+    }
+}
