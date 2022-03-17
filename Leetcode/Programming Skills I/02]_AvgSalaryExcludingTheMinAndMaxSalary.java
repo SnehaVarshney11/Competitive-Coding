b04493f0caff803d31fd09d782924dfa924e1a2e
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-03-17 18:36:15 +0530

    Average Salary Excluding the Minimum and Maximum Salary

---
+++ Leetcode/Programming Skills I/02]_AvgSalaryExcludingTheMinAndMaxSalary.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+class AvgSalaryExcludingTheMinAndMaxSalary {
+    public double average(int[] salary) {
+        double sum = 0;
+        Arrays.sort(salary);
+        salary[0] = 0;
+        salary[salary.length-1] = 0;
+        for(int i = 0; i < salary.length; i++){
+            sum += salary[i];
+        }
+        return (sum/(salary.length-2));
+    }
+}
