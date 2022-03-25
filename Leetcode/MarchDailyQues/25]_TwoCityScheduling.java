63bcf19e6c7b2b71133234b05b1b8453cb16798e
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-03-25 19:31:01 +0530

    Two City Scheduling

---
+++ Leetcode/MarchDailyQues/25]_TwoCityScheduling.java
@@ -0,0 +1,20 @@
+package MarchDailyQues;
+
+import java.util.Arrays;
+
+class TwoCityScheduling {
+    public int twoCitySchedCost(int[][] costs) {
+        Arrays.sort(costs, (a, b) -> {
+            return (a[0] - a[1]) - (b[0] - b[1]);
+        });
+        
+        int price = 0;
+        for(int i = 0; i < costs.length / 2; i++){
+            price += costs[i][0];
+        }
+        for(int i = costs.length / 2; i < costs.length; i++){
+            price += costs[i][1];
+        }
+        return price;
+    }
+}
