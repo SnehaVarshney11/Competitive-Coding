c49145c2271e038bceb0e068ab9fa78e3fa162e3
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-04-02 22:29:50 +0530

    Email Reminders

---
+++ codechef/Moengage April Cook Off/Email_Reminders.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+//  Code By Sneha Varshney 
+class Email_Reminders{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int u = sc.nextInt();
+        System.out.println(n-u);
+    }
+}
