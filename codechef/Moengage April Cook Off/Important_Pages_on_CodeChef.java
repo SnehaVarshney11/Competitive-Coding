94d3f1194d0e7d43d8a4d664f2db53a4d658ef29
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-04-02 22:28:38 +0530

    Importanat pages on codechef

---
+++ codechef/Moengage April Cook Off/Important_Pages_on_CodeChef.java
@@ -0,0 +1,18 @@
+//  Code By Sneha Varshney
+import java.util.Scanner;
+
+class Important_Pages_on_CodeChef{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt(); // user has submitted on the practice page
+        int B = sc.nextInt(); // user has participated in a contest
+
+        if(A == 1 && B == 1){
+            System.out.println("https://discuss.codechef.com");
+        }else if(A == 0){
+            System.out.println("https://www.codechef.com/practice");
+        }else{
+            System.out.println("https://www.codechef.com/contests");
+        }
+    }
+}
