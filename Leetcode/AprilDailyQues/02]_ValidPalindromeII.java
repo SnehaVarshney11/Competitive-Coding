cde47d088504cf03b6ee75e418ec78d24a9f8b79
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-04-02 22:36:35 +0530

    Valid Palindrome II

---
+++ Leetcode/AprilDailyQues/02]_ValidPalindromeII.java
@@ -0,0 +1,28 @@
+package AprilDailyQues;
+
+class ValidPalindromeII {
+    public boolean isPalindrome(String s, int i, int j){
+        while(i <= j){
+            if(s.charAt(i) == s.charAt(j)){
+                i++;
+                j--;
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+    public boolean validPalindrome(String s) {
+        int i = 0, j = s.length()-1;
+        
+        while(i <= j){
+            if(s.charAt(i) == s.charAt(j)){
+                i++;
+                j--;
+            }else{
+                return isPalindrome(s, i+1, j) || isPalindrome(s, i, j-1);
+            }
+        }
+        return true;
+    }
+}
