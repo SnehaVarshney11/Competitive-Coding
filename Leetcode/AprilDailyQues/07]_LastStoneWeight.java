4155b1fbfa762286eb403c3cbc4a31d2e17fd774
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-04-07 21:13:04 +0530

    Last Stone Weight

---
+++ Leetcode/AprilDailyQues/07]_LastStoneWeight.java
@@ -0,0 +1,21 @@
+package AprilDailyQues;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+class LastStoneWeight {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+         for(int st: stones)
+             pq.offer(st);
+     
+         while(pq.size() > 1) {
+             int f = pq.poll();
+             int s = pq.poll();
+             if(f != s) 
+                 pq.offer(f-s);
+         }
+     
+         return pq.isEmpty() ? 0 : pq.peek(); 
+     }
+}
