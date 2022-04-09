96e168ec78cb02bcf5f85b2c97ef2e2e4ea08943
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-04-09 21:57:01 +0530

    Top K Frequent Elements

---
+++ Leetcode/AprilDailyQues/09]_TopKFrequentElements.java
@@ -0,0 +1,38 @@
+package AprilDailyQues;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class TopKFrequentElements {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for(int i : nums){
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+        
+        List<Integer> bucket[] = new ArrayList[nums.length + 1];
+        
+        for(int key : map.keySet()){
+            int freq = map.get(key);
+            if(bucket[freq] == null){
+                bucket[freq] = new ArrayList<>();
+            }
+            bucket[freq].add(key);
+        }
+        
+        int res[] = new int[k];
+        int index = 0;
+        for(int i = bucket.length - 1; i >= 0; i--){
+            if(bucket[i] != null){
+                for(int val : bucket[i]){
+                    res[index++] = val;
+                    if(index == k) return res;
+                }
+            }
+        }
+        return res;
+    }
+}
