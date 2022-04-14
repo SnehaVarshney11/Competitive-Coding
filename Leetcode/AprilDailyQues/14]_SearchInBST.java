50952d06b905617cdba2e7c80279934b412e27dc
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-04-14 12:46:02 +0530

    Search in a Binary Search Tree

---
+++ Leetcode/AprilDailyQues/14]_SearchInBST.java
@@ -0,0 +1,27 @@
+package AprilDailyQues;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+class SearchInBST {
+    public TreeNode searchBST(TreeNode root, int val) {
+        while(root != null){
+            if(root.val > val) 
+                root = root.left;
+            else if(root.val < val)
+                root = root.right;
+            else 
+                return root;
+        }
+        return root;
+    }
+}
