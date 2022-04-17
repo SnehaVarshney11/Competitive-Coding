34810a138f33c97152a5c0813ff20b8de11fe2af
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-04-17 23:36:47 +0530

    Maximum Depth of Binary Tree

---
+++ Recursion/03]_MaximumDepthofBinaryTree.java
@@ -0,0 +1,10 @@
+package Recursion;
+
+class MaximumDepthofBinaryTree {
+    public int maxDepth(TreeNode root) {
+        if(root == null) return 0;
+        int lh = maxDepth(root.left);
+        int rh = maxDepth(root.right);
+        return 1 + Math.max(lh, rh);
+    }
+}
