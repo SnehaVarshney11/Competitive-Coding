9dabb1870c66aa91f9dca66859f334229d682881
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-04-17 21:08:25 +0530

    Two Mirror Trees

---
+++ Amazon Questions/Tree/19]_TwoMirrorTrees.java
@@ -0,0 +1,13 @@
+package Amazon Questions.Tree;
+
+class TwoMirrorTrees {
+    boolean areMirror(Node a, Node b) {
+        // Your code here
+        if(a == null || b == null) return a==b;
+        
+        if(a.data != b.data){
+            return false;
+        }
+        return areMirror(a.left, b.right) && areMirror(a.right, b.left);
+    }
+}
