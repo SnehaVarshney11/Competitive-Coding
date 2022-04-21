2a526066a93ae5bbc19f7ec8f68cd7ffa5c76e3d
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-04-21 21:51:11 +0530

    Design HashSet

---
+++ Leetcode/AprilDailyQues/21]_DesignHashSet.java
@@ -0,0 +1,21 @@
+package Leetcode.AprilDailyQues;
+
+class DesignHashSet {
+    int size = (int)Math.pow(10, 6)+1;
+	boolean[] flag;
+    public MyHashSet() {
+		flag = new boolean[size];
+	}
+    
+    public void add(int key) {
+        flag[key]=true;
+    }
+    
+    public void remove(int key) {
+        flag[key]=false;
+    }
+    
+    public boolean contains(int key) {
+        return flag[key];
+    }
+}
