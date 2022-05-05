2530cc53e24da25c33f1c4ca63c67f5cb983ead3
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-05-05 11:38:00 +0530

    Trie

--- Trie/Trie.java
+++ Trie/Trie.java
@@ -31,14 +31,37 @@ class Trie{
         }
         cur.isTerminal = true;
     }
-    public void search(String word){
-        
+    public boolean search(String word){
+        Node cur = this.root;
+        for(int i = 0; i < word.length(); i++){
+            char ch = word.charAt(i);
+            if(!cur.children.containsKey(ch)){
+                return false;
+            }else{
+                cur = cur.children.get(ch);
+            }
+        }
+        return cur.isTerminal;
+    }
+    public boolean startsWith(String prefix){
+        Node cur = this.root;
+        for(int i = 0; i < prefix.length(); i++){
+            char ch = prefix.charAt(i);
+            if(!cur.children.containsKey(ch)){
+                return false;
+            }else{
+                cur = cur.children.get(ch);
+            }
+        }
+        return true;
     }
     public static void main(String[] args){
         Trie t = new Trie();
         t.insert("Apple");
-        t.insert("App");
         t.insert("Mango");
-        System.out.println(t);
+        t.insert("Mango");
+        System.out.println(t.search("Apple"));
+        System.out.println(t.search("App"));
+        System.out.println(t.startsWith("Go"));
     }
 }
