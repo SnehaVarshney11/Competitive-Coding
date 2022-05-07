ef0549bf1c923996529f4906ee3ae43f68004b49
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-05-07 21:36:58 +0530

    Merge two sorted linked lists

---
+++ GFG Daily Questions/May/07]_MergeTwoSortedLinkedLists.java
@@ -0,0 +1,29 @@
+class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }
+class MergeTwoSortedLinkedLists {
+    Node sortedMerge(Node head1, Node head2) {
+        // This is a "method-only" submission. 
+        // You only need to complete this method
+           if(head1 == null) return head2;
+           if(head2 == null) return head1;
+           
+           Node ll = new Node(0);
+           Node cur = ll;
+           while(head1 != null && head2 != null){
+               if(head1.data <= head2.data){
+                   cur.next = head1;
+                   head1 = head1.next;
+               }else{
+                   cur.next = head2;
+                   head2 = head2.next;
+               }
+               cur = cur.next;
+           }
+           cur.next = head1 == null ? head2 : head1; 
+           return ll.next;
+      } 
+}
