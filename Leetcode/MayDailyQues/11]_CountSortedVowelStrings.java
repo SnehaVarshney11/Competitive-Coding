64d48ca9e4706c9174325fc8ea2cab07480cb52b
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-05-11 22:17:33 +0530

    Count Sorted Vowel Strings

---
+++ Leetcode/MayDailyQues/11]_CountSortedVowelStrings.java
@@ -0,0 +1,17 @@
+package Leetcode.MayDailyQues;
+
+class CountSortedVowelStrings {
+    int a=1, e=1, i=1, o=1, u=1;
+    public int countVowelStrings(int n) {
+        for(int j=1; j<n; j++){
+        	increment();
+        }
+    	return a+e+i+o+u;
+    }
+    private void increment() {
+		a = a+e+i+o+u;
+		e = e+i+o+u;
+		i = i+o+u;
+		o = o+u;
+	}
+}
