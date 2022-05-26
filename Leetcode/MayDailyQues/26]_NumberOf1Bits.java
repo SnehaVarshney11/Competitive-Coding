ee2491e43a10d35a4e097ad9f052720b3ab95d5b
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-05-26 22:12:46 +0530

    Number of 1 Bits

---
+++ Leetcode/MayDailyQues/26]_NumberOf1Bits.java
@@ -0,0 +1,15 @@
+package Leetcode.MayDailyQues;
+
+class NumberOf1Bits {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int count = 0;
+        // The java.lang.Integer.toBinaryString() method returns a string representation of the integer argument as an unsigned integer in base 2.
+        char[] ar = Integer.toBinaryString(n).toCharArray();
+        for(char c : ar){
+            if(c == '1')
+                count++;
+        }
+        return count;
+    }
+}
