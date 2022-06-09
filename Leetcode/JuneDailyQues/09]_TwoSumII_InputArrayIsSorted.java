ba6b802dd430074a8186c8d67479b14c334c7b4c
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-06-09 22:27:45 +0530

    Two Sum II - Input Array Is Sorted

---
+++ Leetcode/JuneDailyQues/09]_TwoSumII_InputArrayIsSorted.java
@@ -0,0 +1,19 @@
+package Leetcode.JuneDailyQues;
+
+class TwoSum2InputArrayIsSorted {
+    public int[] twoSum(int[] numbers, int target) {
+        int a = 0, b = numbers.length-1;
+        
+        while(a<=b){
+            int sum = numbers[a]+numbers[b];
+            if(sum>target){
+                b = b-1;
+            } else if(sum<target){
+                a = a+1;
+            } else{
+                return new int[] {a+1,b+1};
+            }
+        }
+        return new int[] {a+1,b+1};
+    }
+}
