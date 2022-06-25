274b8f7c9e9b1520d55e12dc83f00e52c59a214f
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-06-25 10:21:38 +0530

    Count possible ways to construct buildings

---
+++ GFG Daily Questions/June/23]_CountPossibleWaysToConstructBuildings.java
@@ -0,0 +1,20 @@
+package June;
+
+class CountPossibleWaysToConstructBuildings {
+    public int TotalWays(int N)
+   {
+       long a = 1;
+       long b = 1;
+       long c = 0;
+       
+       for(int i=0;i<N;i++){
+           c = (a + b)%1000000007;
+           a = b%1000000007;
+           b = c%1000000007;
+       }
+       c = c*c;
+       c = c % 1000000007;
+       int z=(int)c;  
+       return z;
+   }
+}
