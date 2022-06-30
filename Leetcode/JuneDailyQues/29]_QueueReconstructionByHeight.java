5bcddd099db9a85b072d4aecf9f624f5b3ae1c04
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-06-30 13:47:36 +0530

    Queue Reconstruction by Height

---
+++ Leetcode/JuneDailyQues/29]_QueueReconstructionByHeight.java
@@ -0,0 +1,17 @@
+package Leetcode.JuneDailyQues;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+class QueueReconstructionByHeight {
+    public int[][] reconstructQueue(int[][] people) {
+        Arrays.sort(people, (a,b) -> a[0] == b[0] ? a[1] - b[1] : b[0] - a[0]);
+
+	    List<int[]> ordered = new LinkedList<>();
+        
+	    for (int[] p: people) ordered.add(p[1], p);
+        
+	    return ordered.toArray(new int[people.length][2]);
+    }
+}
