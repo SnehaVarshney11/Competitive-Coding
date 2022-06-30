dc0be91412232ee99074c998503a704d289b53c8
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-06-30 13:50:22 +0530

    Jump Game

--- GFG Daily Questions/June/29]_JumpGame.java
+++ GFG Daily Questions/June/29]_JumpGame.java
@@ -3,8 +3,7 @@ package June;
 class JumpGame{
     static int canReach(int[] A, int N) {
         // code here
-        
-    int left = N-2;
+        int left = N-2;
         int right = N-1;
         while(left>=0&&right>=0) {
             if(right-left<=A[left]) {
