3e48235c6e182c1bbb5b3c20fe886f9a16921b2a
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-07-05 19:02:07 +0530

    Count BST nodes that lie in a given range

---
+++ GFG Daily Questions/July/05]_CountBSTNodesthatLieInAGivenRange.java
@@ -0,0 +1,23 @@
+package July;
+
+class Node{
+    int data;
+    Node left, right;
+}
+class CountBSTNodesthatLieInAGivenRange {
+    int getCount(Node root,int l, int h)
+    {
+        //Your code here
+        if(root == null){
+            return 0;
+        }
+        int left = getCount(root.left, l, h);
+        int right = getCount(root.right, l, h);
+        
+        if(root.data >= l && root.data <= h){
+            return left+right+1;
+        }else{
+            return left+right;
+        }
+    }
+}
