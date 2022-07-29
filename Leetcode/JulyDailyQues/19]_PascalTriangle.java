eadaf2e9451b71ee6a50437efe6dfcf18d0574c7
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-07-29 22:27:42 +0530

    Pascal's Triangle

---
+++ Leetcode/JulyDailyQues/19]_PascalTriangle.java
@@ -0,0 +1,25 @@
+package Leetcode.JulyDailyQues;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class PascalTriangle {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> list = new ArrayList<List<Integer>>();
+        List<Integer> row, pre = null;
+        
+        for(int i = 0; i < numRows; i++){
+            row = new ArrayList<Integer>();
+            for(int j = 0; j <= i; j++){
+                if(j == 0 || j == i){
+                    row.add(1);
+                }else{
+                    row.add(pre.get(j-1) + pre.get(j));
+                }
+            }
+            pre = row;
+            list.add(row);
+        }
+        return list;
+    }
+}
