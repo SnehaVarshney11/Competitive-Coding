ef0bafedd50f2b28ef1cd52e5f0b6a088cf4cf5d
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-08-04 22:31:45 +0530

    Mirror Reflection

---
+++ GFG Daily Questions/August/04]_MirrorReflection.java
@@ -0,0 +1,17 @@
+package August;
+
+class MirrorReflection {
+    public int mirrorReflection(int p, int q) {
+        while ((p%2==0) && (q%2==0)) {
+            p/=2;
+            q/=2;
+        }
+        if((p%2)==0 && (q%2)!=0){
+            return 2;
+        } 
+        if((p%2)!=0 && (q%2)!=0){
+            return 1;
+        } 
+        return 0;
+    }
+}
