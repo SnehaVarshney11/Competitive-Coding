ed7583ea87c495f9ad071460b8053f3da96fe8fc
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-08-04 22:34:36 +0530

    My Calendar I

---
+++ Leetcode/AugDailyQues/03]_MyCalendar.java
@@ -0,0 +1,21 @@
+package Leetcode.AugDailyQues;
+
+import java.util.TreeMap;
+
+class MyCalendar {
+    TreeMap<Integer, Integer> map;
+    public MyCalendar() {
+        map = new TreeMap<>();
+    }
+
+    public boolean book(int start, int end) {
+        Integer low = map.lowerKey(end);
+
+        if(low == null || map.get(low) <= start) {
+            map.put(start, end);
+            return true;
+        }
+        return false;
+    }
+    
+}
