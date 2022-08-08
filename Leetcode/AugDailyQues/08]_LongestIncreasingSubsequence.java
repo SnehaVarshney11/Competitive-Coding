aea1a962cb7f55c8f62717c07026338bc000733e
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-08-08 21:15:57 +0530

    Longest Increasing Subsequence

---
+++ Leetcode/AugDailyQues/08]_LongestIncreasingSubsequence.java
@@ -0,0 +1,24 @@
+package Leetcode.AugDailyQues;
+
+class LongestIncreasingSubsequence {
+    public int lengthOfLIS(int[] nums) {
+        //Base case
+        if(nums.length == 0) return 0;
+        
+        int[] dp = new int[nums.length];
+        dp[0] = 1;
+        int ans = 1;
+        
+        for(int i = 1; i < dp.length; i++){
+            int max = 0;
+            for(int j = 0; j < i; j++){
+                if(nums[i] > nums[j]){
+                    max = Math.max(max, dp[j]);
+                }
+            }
+            dp[i] = max + 1;
+            ans = Math.max(ans, dp[i]);
+        }
+        return ans;
+    }
+}
