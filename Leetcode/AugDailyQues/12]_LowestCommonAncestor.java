69644b3d456b15293b163ac44ee052ec75cc33e6
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-08-12 21:23:47 +0530

    Lowest Common Ancestor of a Binary Search Tree

---
+++ Leetcode/AugDailyQues/12]_LowestCommonAncestor.java
@@ -0,0 +1,21 @@
+package Leetcode.AugDailyQues;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+class LowestCommonAncestor {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root.val > p.val && root.val > q.val){
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        else if(root.val < p.val && root.val < q.val){
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        else{
+            return root;
+        }
+    }
+}
