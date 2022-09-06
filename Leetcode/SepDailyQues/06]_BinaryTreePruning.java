f72be2b6ff27573b2fbab0ae98d25182fa3e7398
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-09-06 22:07:02 +0530

    Binary Tree Pruning

---
+++ Leetcode/SepDailyQues/06]_BinaryTreePruning.java
@@ -0,0 +1,23 @@
+package Leetcode.SepDailyQues;
+
+class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+    }
+}
+class BinaryTreePruning{
+    public TreeNode pruneTree(TreeNode root) {
+        if (root == null) return null;
+        root.left = pruneTree(root.left);
+        root.right = pruneTree(root.right);
+        if (root.left == null && root.right == null && root.val == 0) return null;
+        return root;
+    }
+}
