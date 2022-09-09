cf865eadbede61eb1c38f3ad1cbce1451bce32bf
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-09-09 21:10:25 +0530

    Inorder Traversal

--- Leetcode/SepDailyQues/08]_BinaryTreeInorderTraversal.java
+++ Leetcode/SepDailyQues/08]_BinaryTreeInorderTraversal.java
@@ -3,6 +3,10 @@ package Leetcode.SepDailyQues;
 import java.util.ArrayList;
 import java.util.List;
 
+class TreeNode{
+	TreeNode left, right;
+	int val;
+}
 class BinaryTreeInorderTraversal {
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> list = new ArrayList<>();
