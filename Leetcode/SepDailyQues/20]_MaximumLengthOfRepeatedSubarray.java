35ee0f7d175343f394c4026abc545750b2bf88ac
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-09-20 20:20:22 +0530

    Maximum Length of Repeated Subarray

---
+++ Leetcode/SepDailyQues/20]_MaximumLengthOfRepeatedSubarray.java
@@ -0,0 +1,23 @@
+package Leetcode.SepDailyQues;
+
+class MaximumLengthOfRepeatedSubarray {
+    public int findLength(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        int m = nums2.length;
+        int res = 0;
+        
+        int dp[][] = new int[n+1][m+1];
+        
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                if(nums1[i-1] == nums2[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                    res = Math.max(res, dp[i][j]);
+                }else{
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        return res;
+    }
+}
