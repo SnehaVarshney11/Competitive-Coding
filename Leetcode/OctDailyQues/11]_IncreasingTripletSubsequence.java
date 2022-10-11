0145df3a776d8cc0e82aef3c241d9c96aaf46939
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-10-11 23:00:29 +0530

    Increasing Triplet Subsequence

---
+++ Leetcode/OctDailyQues/11]_IncreasingTripletSubsequence.java
@@ -0,0 +1,14 @@
+package Leetcode.OctDailyQues;
+
+class IncreasingTripletSubsequence {
+    public boolean increasingTriplet(int[] nums) {
+        int max1 = Integer.MAX_VALUE;
+       int max2 = Integer.MAX_VALUE;
+       for(int n : nums) {
+           if(n <= max1) max1 = n;
+           else if(n <= max2) max2 = n;
+           else return true;
+       }
+       return false;
+    }
+}
