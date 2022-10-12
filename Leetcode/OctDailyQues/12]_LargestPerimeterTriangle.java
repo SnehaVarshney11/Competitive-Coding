520b6f9f42b45ebfaad90c7f18a26da68a5045c6
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-10-12 22:49:58 +0530

    Largest Perimeter Triangle

--- Leetcode/OctDailyQues/12]_LargestPerimeterTriangle.java
+++ Leetcode/OctDailyQues/12]_LargestPerimeterTriangle.java
@@ -1,5 +1,19 @@
 package Leetcode.OctDailyQues;
 
+import java.util.Arrays;
+
 class LargestPerimeterTriangle {
-    
+    public int largestPerimeter(int[] nums) {
+        Arrays.sort(nums);
+        for(int i = nums.length-3; i >= 0; i--){
+            if(isPossible(nums[i], nums[i+1], nums[i+2])){
+                return nums[i] + nums[i+1] + nums[i+2];
+            }
+        }
+        return 0;
+    }
+    boolean isPossible(int a, int b, int c){
+        double s = (a+b+c)/2.0;
+        return s*(s-a)*(s-b)*(s-c) > 0;
+    }
 }
