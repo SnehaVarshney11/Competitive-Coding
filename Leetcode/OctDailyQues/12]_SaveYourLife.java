b995247425dd9a4c9a1eb3a42b82b0114c17723e
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-10-12 22:47:39 +0530

    Save Your Life

---
+++ Leetcode/OctDailyQues/12]_SaveYourLife.java
@@ -0,0 +1,43 @@
+package Leetcode.OctDailyQues;
+
+import java.util.HashMap;
+
+class SaveYourLife {
+    static String maxSum(String w,char x[],int b[], int n){
+        //code here
+        HashMap<Character,Integer> uwu=new HashMap<>();
+        for(int i=0;i<n;i++)
+        {
+            uwu.put(x[i],b[i]);
+        }
+        
+        StringBuilder temp=new StringBuilder();
+        StringBuilder ans=new StringBuilder();
+        
+        
+        int max=Integer.MIN_VALUE;
+        int curr=0;
+        for(char c:w.toCharArray())
+        {
+            curr+=uwu.containsKey(c)?uwu.get(c):c;
+            temp.append(c);
+            
+            
+            if(curr>max)
+            {
+                  max=curr;
+                  ans=new StringBuilder(temp);
+            }
+          
+            
+            
+            if(curr<0)
+            {
+                curr=0;
+                temp=new StringBuilder();
+            }
+        }
+        
+        return ans.toString();
+    }
+}
