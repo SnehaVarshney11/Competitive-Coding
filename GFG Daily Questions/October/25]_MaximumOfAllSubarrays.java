e7af4eece433f7d9088acd047e0a7bb3addc2704
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-10-25 16:28:19 +0530

    Maximum of all subarrays of size k

---
+++ GFG Daily Questions/October/25]_MaximumOfAllSubarrays.java
@@ -0,0 +1,35 @@
+package October;
+
+import java.util.ArrayList;
+
+class MaximumOfAllSubarrays {
+    static ArrayList <Integer> max_of_subarrays(int arr[], int n, int k)
+    {
+        // Your code here
+        ArrayList<Integer> a = new ArrayList<Integer>();
+
+        for(int i=0;i<=n-k;i++)
+
+        {
+
+            int max =0;
+
+            for(int j=i;j<k+i;j++)
+
+            {
+
+                if(arr[j]>max)
+
+                max = arr[j];
+
+            }
+
+            a.add(max);
+
+        }
+
+        return a;
+
+        
+    }
+}
