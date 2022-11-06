0cde68a9f3ba3f954602a017705c2ec6075cb871
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-11-06 22:22:47 +0530

    Orderly Queue

---
+++ Leetcode/NovemberDaily/06]_OrderlyQueue.java
@@ -0,0 +1,22 @@
+package Leetcode.NovemberDaily;
+
+import java.util.Arrays;
+
+class OrderlyQueue {
+    public String orderlyQueue(String s, int k) {
+        if(k>1){
+            char tempArray[] = s.toCharArray();
+            Arrays.sort(tempArray);
+            return new String(tempArray);
+        } else {
+            String ans  = s;
+            for(int i=0;i< s.length(); i++){
+                s = s.substring(1) + s.substring(0,1);
+                if(ans.compareTo(s) >0){
+                    ans = s;
+                }
+            }
+            return ans;
+        }
+    }
+}
