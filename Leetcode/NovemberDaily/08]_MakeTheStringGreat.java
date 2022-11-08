a29cbc9dd6e9ff7534567df238704d74e5523172
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-11-08 22:30:45 +0530

    Make The String Great

---
+++ Leetcode/NovemberDaily/08]_MakeTheStringGreat.java
@@ -0,0 +1,19 @@
+package Leetcode.NovemberDaily;
+
+class MakeTheStringGreat {
+    public String makeGood(String s) {
+        StringBuilder sb = new StringBuilder();
+        int pt2 = 0;
+        for(int i = 0; i<s.length(); i++){
+            if(pt2>0 && Math.abs(sb.charAt(pt2-1) - s.charAt(i)) == 32){
+                pt2--;
+                sb.deleteCharAt(pt2);
+            }
+            else{
+                sb.append(s.charAt(i));
+                pt2++;
+            }
+        }
+        return sb.toString();
+    }
+}
