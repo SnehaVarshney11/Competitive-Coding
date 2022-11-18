8fa8dc92819bc1ef7485e33b26b740261c40d99e
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-11-18 21:10:09 +0530

    Ugly Number

--- Leetcode/NovemberDaily/18]_UglyNumber.java
+++ Leetcode/NovemberDaily/18]_UglyNumber.java
@@ -1,3 +1,12 @@
+package Leetcode.NovemberDaily;
+
 class UglyNumber {
-    
+    public boolean isUgly(int n) {
+        if(n == 0)return false;
+        if(n == 1)return true;
+        if(n%2 == 0) return isUgly(n/2);
+        else if(n%3 == 0) return isUgly(n/3);
+        else if(n%5 == 0) return isUgly(n/5);
+        return false;
+    }
 }
