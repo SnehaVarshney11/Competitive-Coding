cfb711e57bc98835731ad9098f9d840bd5919c5b
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-11-23 18:06:05 +0530

    Valid Sudoku

---
+++ Leetcode/NovemberDaily/23]_ValidSudoku.java
@@ -0,0 +1,20 @@
+package Leetcode.NovemberDaily;
+
+import java.util.HashSet;
+import java.util.Set;
+
+class ValidSudoku {
+    public boolean isValidSudoku(char[][] board) {
+        Set seen = new HashSet<>();
+        for (int i=0; i<9; ++i) {
+        for (int j=0; j<9; ++j) {
+            if (board[i][j] != '.') {
+                String b = "(" + board[i][j] + ")";
+                if (!seen.add(b + i) || !seen.add(j + b) || !seen.add(i/3 + b + j/3))
+                    return false;
+            }
+        }
+    }
+    return true;
+    }
+}
