12d5594aabf3d989e708a07f56b71ebd44753b00
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-12-03 21:46:51 +0530

    String Halves

---
+++ Leetcode/DecDaily/01]_StringHalves.java
@@ -0,0 +1,18 @@
+package Leetcode.DecDaily;
+
+import java.util.Set;
+
+class StringHalves {
+    public boolean halvesAreAlike(String s) {
+        Set<Character> set = Set.of('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U');
+        int cnt = 0, in = s.length() / 2;
+
+        for (int i = 0; i < in; i++) {
+            char charA = s.charAt(i);
+            char charB = s.charAt(in + i);
+            if (set.contains(charA)) cnt++;
+            if (set.contains(charB)) cnt--;
+        }
+        return cnt == 0;
+    }
+}
