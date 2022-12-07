f55fbe0b60ac24d1d7ad389330fe5e9f384ed5ad
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-12-07 22:56:29 +0530

    Range Sum of BST

---
+++ Leetcode/DecDaily/07]_RangeSum.java
@@ -0,0 +1,25 @@
+package Leetcode.DecDaily;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+class RangeSum {
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if(root == null)
+                return 0;
+            if(root.val > high)
+                return rangeSumBST(root.left, low, high);
+            if(root.val < low)
+                return rangeSumBST(root.right, low, high);
+            return root.val + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
+        }
+}
