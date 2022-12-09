0f43e015bd8dd71eeddc6d997c5abdb32e406b4b
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-12-09 23:43:19 +0530

    Maximum Difference Between Node and Ancestor

--- Leetcode/DecDaily/09]_MaxDiffBetweenNodeAndAncestor.java
+++ Leetcode/DecDaily/09]_MaxDiffBetweenNodeAndAncestor.java
@@ -1,5 +1,15 @@
 package Leetcode.DecDaily;
 
 class MaxDiffBetweenNodeAndAncestor {
-    
+    public int maxAncestorDiff(TreeNode root, int ancestor_min, int ancestor_max) {
+        if (root != null) 
+            return Math.max(maxAncestorDiff(root.left, Math.min(root.val, ancestor_min), Math.max(root.val, ancestor_max)),
+                   maxAncestorDiff(root.right, Math.min(root.val, ancestor_min), Math.max(root.val, ancestor_max)));
+        else 
+            return ancestor_max - ancestor_min;
+    }
+
+    public int maxAncestorDiff(TreeNode root) {
+        return maxAncestorDiff(root, 100000, 0);
+    }
 }
