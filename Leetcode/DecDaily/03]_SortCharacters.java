3474e37d6bb8f51e08d73eb88d611c963b0930e5
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-12-13 17:25:42 +0530

    Sort Characters By Frequency

---
+++ Leetcode/DecDaily/03]_SortCharacters.java
@@ -0,0 +1,27 @@
+package Leetcode.DecDaily;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+class SortCharacters {
+    public String frequencySort(String s) {
+        if (s.length() < 3) return s;
+
+        StringBuilder sb = new StringBuilder();
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char c : s.toCharArray()) map.merge(c, 1, Integer::sum);
+
+        map.entrySet().stream()
+
+                .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+
+                .forEach(entry -> sb.append(String.valueOf(entry.getKey()).repeat(entry.getValue())));
+
+            
+
+        return sb.toString();
+    }
+}
