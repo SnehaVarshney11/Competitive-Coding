d04c487d4f04ba1b15dfc64ec098708d03b14c88
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-12-20 14:48:09 +0530

    Keys and Rooms

--- Leetcode/DecDaily/20]_KeysAndRooms.java
+++ Leetcode/DecDaily/20]_KeysAndRooms.java
@@ -1,5 +1,35 @@
 package Leetcode.DecDaily;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 class KeysAndRooms {
-    
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        boolean[] visited = new boolean[rooms.size()];
+        
+        BFS(rooms,visited);
+        
+        for(int i=0; i<visited.length; i++){
+            if(visited[i] == false) return false;
+        }
+        return true;
+    }
+    private void BFS(List<List<Integer>> rooms,boolean[] visited){
+        Queue<Integer> queue =  new LinkedList<>();
+        visited[0] = true;
+        for(int i : rooms.get(0)){
+            queue.add(i);
+            visited[i] = true;
+        }
+        while(!queue.isEmpty()){
+            int v = queue.poll();
+            for(int i : rooms.get(v)){
+                if(visited[i] == false){
+                    queue.add(i);
+                    visited[i] = true;
+                }
+            }
+        }
+    }
 }
