7368e243b75a6b0f4ec85b60e2f2cdd293532bf6
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-12-29 17:46:42 +0530

    Middle of the Linked List

---
+++ Leetcode/DecDaily/05]_MiddleOfLinkedList.java
@@ -0,0 +1,20 @@
+package Leetcode.DecDaily;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+class MiddleOfLinkedList {
+    public ListNode middleNode(ListNode head) {
+        ListNode a = head;
+        ListNode b = head;
+        while(b!=null && b.next!=null){
+            a = a.next;
+            b = b.next.next;
+        }
+        return a; 
+    }
+}
