e8f4ab8423b6c3774ca67d20b0d938d83585375f
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2023-01-06 17:07:34 +0530

    Maximum Product of Splitted Binary Tree

---
+++ Leetcode/DecDaily/10]_MaximumProduct.java
@@ -0,0 +1,28 @@
+package Leetcode.DecDaily;
+
+class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode() {}
+     TreeNode(int val) { this.val = val; }
+     TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+     }
+ }
+class MaximumProduct {
+    long res = 0, total = 0, sub;
+    public int maxProduct(TreeNode root) {
+        total = s(root); s(root);
+        return (int)(res % (int)(1e9 + 7));
+    }
+
+    private long s(TreeNode root) {
+        if (root == null) return 0;
+        sub = root.val + s(root.left) + s(root.right);
+        res = Math.max(res, sub * (total - sub));
+        return sub;
+    }
+}
