54aceb63f93d1888f7a3a1be9ff4e206164c33cf
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2023-02-17 23:55:19 +0530

    Moves Zero

--- StudyPlan/Algorithm/MovesZeros.java
+++ StudyPlan/Algorithm/MovesZeros.java
@@ -3,6 +3,7 @@ package StudyPlan.Algorithm;
 class MovesZeros{
     public void moveZeroes(int[] nums) {
         int start = 0, end = 0;
+        
         while(end < nums.length){
             if(nums[end] != 0){
                 int temp = nums[end];
