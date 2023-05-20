d23740f054beabef915d1b535a51977af9ca9fd9
SnehaVarshney11 <sneha.varshney_cs19@gla.ac.in>
2023-05-20 10:24:55 +0530

    commit

--- codechef/April Long Challenge/Ezio_and_Guards.java
+++ codechef/April Long Challenge/Ezio_and_Guards.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 // solution --> @varsneha
-h
+
 class Ezio_and_Guards{
     public static void main(String[] args) throws Exception {
         InputReader sc = new InputReader(System.in);
